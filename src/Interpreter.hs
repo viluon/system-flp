ad8aae550c4ad9c11c55d75c66b7a3a5a26ed258
Andrew Kvapil <viluon@espiv.net>
2021-05-26 15:41:42 +0200

    Implement an incomplete typechecker

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,31 +1,45 @@
 module Interpreter (
   main
+, helper
 ) where
 
 import System.IO ( stdout, hFlush )
-import Data.Either
+import Debug.Trace ( trace )
 
 import Syntax.AST
 import Syntax.Command
+import Context
 import Eval ( eval )
 import Parser.Parser ( parseExpr )
 import Typechecking
 import Utils
 
 checkAndEval :: TermCheck -> Either Error String
-checkAndEval term = do
-  tp <- typeOf term
-  () <- check term tp
+checkAndEval term' = do
+  tp' <- typeOf term'
+  let tp = typeSubst "Nat" TyNat tp'
+  -- let term = TermCheckInf $ TermTyApp (TermTyLam "Nat" (TermAnn term' tp)) TyNat
+  let term = term'
+  () <- check [("Nat", HasKind Star)] term tp
   return $ show $ eval [] term
 
 helper expr =
   case parseExpr expr of
     Left _     -> error "cmd"
-    Right term -> typeOf term
+    Right term | trace (show term) True -> checkAndEval term
+    _ -> undefined
 
 
--- >>> helper "(/\\ T. (/\\ T. (\\x -> x) :: T))[Int]"
--- Right (TyForall "T" (TyFree "T"))
+-- >>> helper "(/\\ T. (/\\ g. (\\x -> x) :: T -> T))[Int]"
+-- Right "TyLam \"T\" (TermAnn (TermCheckLam \"x\" (TermCheckInf (TermBound 0 \"x\"))) (TyFun (TyFree \"T\") (TyFree \"T\"))) []"
+
+-- >>> helper "(/\\ Nat . (\\x -> x) :: Nat -> Nat)[t]"
+-- Right "Lam \"x\" (TermCheckInf (TermBound 0 \"x\")) []"
+
+-- >>> helper "(/\\ t . (\\x -> x) :: String -> t)[String]"
+-- Left "Expected TyFree \"t\", got TyFree \"String\""
+
+
 
 main :: IO ()
 main = go []
--- src/Typechecking.hs
+++ src/Typechecking.hs
@@ -3,16 +3,54 @@
 module Typechecking (
   check
 , typeOf
+, typeSubst
 ) where
 
 import Syntax.AST
 import Context
 import Utils
 
+check :: Context -> TermCheck -> Type -> Either Error ()
+check ctx (TermCheckLam var body) tp = do (a, b) <- case tp of
+                                            TyFun a b -> Right (a, b)
+                                            _         -> Left $ "Expected type " ++ show tp ++ ", got function"
+                                          check ((var, HasType a):ctx) body b
+check ctx (TermCheckInf term)     tp = do inferred <- infer ctx term
+                                          if tp == inferred then
+                                            Right ()
+                                          else
+                                            Left $ "Expected " ++ show tp ++ ", got " ++ show inferred
 
-check :: TermCheck -> Type -> Either Error ()
-check (TermCheckLam var body) tp = undefined
-check (TermCheckInf term)     tp = undefined
+-- (\x -> x) :: Nat
+
+isValue :: (a, Info) -> Bool
+isValue (_, HasType _) = True
+isValue _              = False
+
+infer :: Context -> TermInfer -> Either Error Type
+infer ctx (TermAnn   term  tp)   = check ctx term tp >> pure tp
+infer _   (TermNat   _)          = Right TyNat
+infer _   (TermFree  name)       = Left $ "Undefined reference to " ++ show name -- unbound variable, TODO
+infer ctx (TermBound n     name) = case filter isValue ctx !!? n of
+                                     Just (_, HasType t) -> Right t
+                                     Just (_, _)         -> error "bug in isValue"
+                                     Nothing             -> Left $ "Undefined reference to " ++ show name
+infer ctx (TermApp   lam   arg)  = do tp <- infer ctx lam
+                                      (a, b) <- case tp of
+                                        TyFun a b -> Right (a, b)
+                                        _         -> Left $ "Expected function type, got " ++ show tp
+                                      check ctx arg a
+                                      pure b
+infer ctx (TermTyLam tvar  body) = do tp <- infer ((tvar, HasKind Star):ctx) body
+                                      pure $ TyForall tvar tp
+infer ctx (TermTyApp tlam  targ) = do tp <- infer ctx tlam
+                                      case tp of
+                                        TyForall nm forAll -> Right $
+                                          typeSubst {- FIXME this is an issue! We need to substitute in the entire AST -}
+                                            nm targ forAll
+                                        _                  -> Left $
+                                          "Expected expression of forall type, got expression of type " ++ show tp
+
 
 class HasType a where
   typeOf :: a -> Either Error Type
@@ -25,7 +63,7 @@ instance HasType TermCheck where
 
 instance HasType TermInfer where
   typeOf (TermAnn   _     tp)   = Right tp
-  typeOf (TermNat   _)          = Right TyNat
+  typeOf (TermNat   _)          = Right $ TyFree "Nat"
   typeOf (TermFree  name)       = Left $ "Undefined reference to " ++ name
   typeOf (TermBound n     _)    = error "the impossible happened!"
   typeOf (TermApp   lam   _)    = typeOf lam >>= \case
